7a79037f999458022f97231002f4ee6d90b53c29
Laura Tejada <laura_mtc@hotmail.com>
2020-07-19 17:52:22 -0500

    adding example using context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, setBlogPosts] = useState([]),
+    addBlogPost = () => {
+      setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+    };
+
+  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+    {children}
+  </BlogContext.Provider>;
+};
+
+export default BlogContext;
