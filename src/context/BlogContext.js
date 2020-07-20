22714add175c2f6c4f0f03aa6d6f83282bb4a394
Laura Tejada <laura_mtc@hotmail.com>
2020-07-20 16:44:36 -0500

    refactor using useReduce hook

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost':
+      return [...state, { title: `Blog Post #${state.length + 1}` }];
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]),
-    addBlogPost = () => {
-      setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
-    };
-
-  return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-    {children}
-  </BlogContext.Provider>;
+    default:
+      return state;
+  }
 };
 
-export default BlogContext;
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: 'add_blogpost' });
+  };
+};
+
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPost },
+  []
+);
